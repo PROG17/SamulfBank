a3dced5eb721832bfb5168e01cb07979c73253cb
Samuel Olofsson Jonsson <JonssonSamuel@hotmail.com>
2018-11-08 12:00:53 +0100

    Classes

---
+++ ALMSamulfBank/BankRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALMSamulfBank
+{
+    public class BankRepository
+    {
+        public List<Customer> Customers { get; set; }
+
+        public BankRepository()
+        {
+            Customers = new List<Customer>();
+        }
+    }
+}
---
+++ ALMSamulfBank/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALMSamulfBank
+{
+    public class Customer
+    {
+        public int           Id       { get; set; }
+        public string        Name     { get; set; }
+        public List<Account> Accounts { get; set; }
+
+        public Customer()
+        {
+            Accounts = new List<Account>();
+        }
+    }
+}
