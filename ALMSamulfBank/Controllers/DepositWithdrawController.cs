8e7b3bf3b5a4b225ed5e8f2896b9efbbfb86be7f
Samuel Olofsson Jonsson <JonssonSamuel@hotmail.com>
2018-11-09 11:19:18 +0100

    DepositWithdrawController/Ajax

---
+++ ALMSamulfBank/Controllers/DepositWithdrawController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ALMSamulfBank.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ALMSamulfBank.Controllers
+{
+    public class DepositWithdrawController : Controller
+    {
+        private BankRepository BankRepo = BankRepository.Instance;
+
+        public IActionResult Index()
+        {
+            return View(BankRepo.Accounts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Deposit(DWViewmodel model)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Withdraw(DWViewmodel model)
+        {
+            return View();
+        }
+
+    }
+}
--- ALMSamulfBank/wwwroot/js/site.js
+++ ALMSamulfBank/wwwroot/js/site.js
@@ -1,1 +1,33 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function () {
+
+
+    $(document).on('click', '#depositBtn', function () {
+
+        var data = {
+            model: {
+                Amount: $('#amountInput').val(),
+                AccountNumber: $('#accountSelect').val()
+            }
+        };
+
+        $.post('/DepositWithdraw/Deposit', data, function (result) {
+        });
+    });
+
+    $(document).on('click', '#withdrawBtn', function () {
+        var data = {
+            model: {
+                Amount: $('#amountInput').val(),
+                AccountNumber: $('#accountSelect').val()
+            }
+        };
+
+        $.post('/DepositWithdraw/Withdraw', data, function (result) {
+        });
+    });
+
+
+});
+
+
+
